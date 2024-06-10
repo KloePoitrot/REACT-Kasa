e03963f0e7e024887d7b9ecadf7530b188d477ba
unknown <KloePoitrot@hotmail.com>
2024-06-10 18:30:56 +0200

    Add logement error id page

--- src/pages/fiche-logement.jsx
+++ src/pages/fiche-logement.jsx
@@ -2,25 +2,34 @@ import Footer from "../components/footer"
 import Header from "../components/header"
 import { useParams } from "react-router-dom"
 import data from '../data/logements.json'
+import ErrorPage from "./error"
 
 function FicheLogement () {
     const { id }  = useParams()
     const logData = data.find(el => el.id === id)
 
-    return (
-        <>
-            <Header />
-            <main>
-                <div>{logData.title}</div>
-                <div>{logData.description}</div>
-                <div>{logData.host.name}</div>
-                <div>{logData.rating}</div>
-                <div>{logData.equipments}</div>
-                <div>{logData.tags}</div>
-            </main>
-            <Footer />
-        </>
-    )
+    if(logData !== undefined){
+        return(
+            <>
+                <Header />
+                    <main>
+                        <div>{logData.title}</div>
+                        <div>{logData.description}</div>
+                        <div>{logData.host.name}</div>
+                        <div>{logData.rating}</div>
+                        <div>{logData.equipments}</div>
+                        <div>{logData.tags}</div>
+                    </main>
+                <Footer />
+            </>
+        )
+    } else {
+        return (
+            <ErrorPage />
+        )
+    }
+    
+    
 }
 
 export default FicheLogement
